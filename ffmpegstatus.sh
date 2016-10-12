eca3666602e77f376e98e7afc8ee47c0862cdfae
Aysad Kozanoglu <aysadk@emailn.de>
2016-10-13 00:58:50 +0200

    fixing the if statement

--- ffmpegstatus.sh
+++ ffmpegstatus.sh
@@ -1,21 +1,28 @@
+
 #! /bin/bash
 #
 # coder: Aysad Kozanoglu
 # email: aysadx@gmail.com
-# web: http://onweb.pe.hu#
+# web: http://onweb.pe.hu
 #
 
 instance=`ps auxf | grep ffmpeg314 | wc -l`
 getinstance=`cat /tmp/ffmpids | wc -l`
+weblog="/usr/local/nginx/html/status.log"
 date=`date "+%F %T"`
+
 echo $instance $getinstance
-if [ ! $instance == $getinstance ];
+echo $instance $getinstance >> $weblog
+
+if [ $instance -ne $getinstance ];
 then
         echo $date" ffmpegstarter instance "$instance" restart" >> /tmp/ffstatus.log
-        echo $date" ffmpeg  instance "$instance" restart" >> /usr/local/nginx/html/live/status.log
+        echo $date" ffmpeg  instance "$instance" restart" >> $weblog
 
-        . /source/ffmpegstarter.sh
+        /source/ffmpegstarter.sh
 else
         echo $date" instance ok">> /tmp/ffstatus.log
-        echo $date" instance ok">>  /usr/local/nginx/html/live/status.log
+        echo $date" "$instance" instance ok">> $weblog
 fi
+
+
