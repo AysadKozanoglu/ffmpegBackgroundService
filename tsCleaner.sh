9d411d9a93eb1277a5acdac98ba8a58d40b4a48f
Aysad Kozanoglu <aysadk@emailn.de>
2016-10-13 22:04:01 +0200

    Update tsCleaner.sh

--- tsCleaner.sh
+++ tsCleaner.sh
@@ -4,7 +4,7 @@
 # email: aysadx@gmail.com
 # web: http://onweb.pe.hu#
 #
-#
+# Clean .ts files older than 10 minutes
 
 echo "ts cleaner started"
 find /usr/local/nginx/html/live -maxdepth 1 -mmin +10 -type f -name "*.ts" \
