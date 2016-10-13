48b0ea72920a1c9890162017d36f4e2e09905605
Aysad Koaznoglu <aysadx@gmail.com>
2016-10-13 22:46:42 +0200

    links.sh added:wq!:

---
+++ links.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+slink[0]="http://daserste_live-lh.akamaihd.net/i/daserste_de@91204/index_2692_av-p.m3u8"
+nlink[0]="daserste"
+slink[1]='http://media.netd.com.tr/S2/HLS_LIVE/cnn_turk/750/prog_index.m3u8'
+nlink[1]='cnnturk'
+slink[2]='http://trtcanlitv-lh.akamaihd.net/i/TRTWORLD_1@321783/index_900_av-b.m3u8'
+nlink[2]='trtworld'
+slink[3]='http://trtcanlitv-lh.akamaihd.net/i/TRTMUZIK_1@181845/index_1200_av-p.m3u8'
+nlink[3]='trt-muzik'
+slink[4]='http://trtcanlitv-lh.akamaihd.net/i/TRTTURK_1@182144/index_1000_av-b.m3u8'
+nlink[4]='trtturk'
+slink[5]='http://trtcanlitv-lh.akamaihd.net/i/TRT1HD_1@181842/index_900_av-b.m3u8'
+nlink[5]='trt1'
+slink[6]='http://trtcanlitv-lh.akamaihd.net/i/TRTHABERHD_1@181942/index_900_av-p.m3u8'
+nlink[6]='trthaber'
+slink[7]='http://trtcanlitv-lh.akamaihd.net/i/TRTBELGESEL_1@182145/index_900_av-b.m3u8'
+nlink[7]='trtbelgesel'
+slink[8]='http://hdiosstv-f.akamaihd.net/i/servustvhdde_1@75540/index_1112_av-p.m3u8'
+nlink[8]='servus-tv'
+slink[9]='http://zdf0910-lh.akamaihd.net/i/dach10_v1@392872/index_1056_av-p.m3u8'
+nlink[9]='3sat'
+slink[10]='http://zdf1314-lh.akamaihd.net/i/de14_v1@392878/index_2256_av-p.m3u8'
+nlink[10]='zdf-hd'
